4de50e779250c73e44c08dcce95f1ca9d1b30c92
dahliar <90829442+dahliar@users.noreply.github.com>
2022-03-22 14:28:15 +0700

    update akses company oleh production

--- resources/views/company/companyAdd.blade.php
+++ resources/views/company/companyAdd.blade.php
@@ -3,7 +3,7 @@
 @extends('layouts.layout')
 
 @section('content')
-@if ((Auth::user()->isAdmin() or Auth::user()->isMarketing()) and Session::has('employeeId') and (Session()->get('levelAccess') <= 3))
+@if ( (Auth::user()->isAdmin() or Auth::user()->isMarketing() or Auth::user()->isProduction() ) and Session::has('employeeId') and (Session()->get('levelAccess') <= 3))
 <script type="text/javascript"> 
     $(document).ready(function() {
         var i=1;
