b134d6a2bc0ad6b052b9cef95f328a27749fba00
dahliar <90829442+dahliar@users.noreply.github.com>
2022-03-28 09:28:40 +0700

    jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html><html lang="en">
+<!--
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+-->
+<script src="../resources/js/jquery-3.6.0.min.js"></script>
+
 <script src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js" type="text/javascript" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/js/bootstrap.bundle.min.js" type="text/javascript"></script>
