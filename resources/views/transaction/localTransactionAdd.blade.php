5816b659573f3140336c39001b65607c871423b6
dahliar <90829442+dahliar@users.noreply.github.com>
2022-03-28 09:55:39 +0700

    Update localTransactionAdd.blade.php

--- resources/views/transaction/localTransactionAdd.blade.php
+++ resources/views/transaction/localTransactionAdd.blade.php
@@ -12,7 +12,57 @@ $pageId = -1;
 
 <script type="text/javascript"> 
     $(document).ready(function() {
+        $('#rekening').on('change', function() {
+            var rek = $(this).val();
+            if (rek>0){
+                $.ajax({
+                    url: '{{ url("getOneRekening") }}'+"/"+rek,
+                    type: "GET",
+                    data : {"_token":"{{ csrf_token() }}"},
+                    dataType: "json",
+                    success:function(data){
+                        if(data){
+                            $('[name="valuta"]').val(data.valuta);
+                        }else{
+                        }
+                    }
+                });
+            }else{
+                swal.fire('warning','Choose Rekening first!','info');
+            }
+        });
+        $('#company').on('change', function() {
+            var company = $(this).val();
+            if (company>0){
+                $.ajax({
+                    url: '{{ url("getOneCompany") }}'+"/"+company,
+                    type: "GET",
+                    data : {"_token":"{{ csrf_token() }}"},
+                    dataType: "json",
+                    success:function(data){
+                        if(data){
+                            $('[name="companydetail"]').val(data.address+'. '+data.nation);
+                        }else{
+                        }
+                    }
+                });
+            }else{
+                $('[name="companydetail"]').val("");
+                swal.fire('warning','Choose Company first!','info');
+            }
+        });
+        $('#valutaType').on('change', function() {
+            var e = document.getElementById("valutaType");
+            var valutaType = e.options[e.selectedIndex].value;
+            var valText = e.options[e.selectedIndex].text;
 
+            if (valutaType>0){
+                document.getElementById("spanAm").textContent=valText;
+                document.getElementById("spanAd").textContent=valText;
+            }else{
+                swal.fire('warning','Choose Payment Valuta first!','info');
+            }
+        });
     });
 </script>
 
@@ -152,7 +202,7 @@ $pageId = -1;
                     <div class="col-md-4">
                         <div class="input-group">
                             <span class="input-group-text col-3">Valuta</span>
-                            <input id="valuta" name="valuta" class="form-control" value="{{ old('valuta') }}">
+                            <input id="valuta" name="valuta" class="form-control" value="{{ old('valuta') }}" readonly>
                         </div>
                     </div>
                 </div>                    
