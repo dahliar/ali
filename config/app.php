fa280ccafec32d465268eed6bc7f2526ec76dbc8
dahliar <90829442+dahliar@users.noreply.github.com>
2022-07-28 13:16:08 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -233,7 +233,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+        'PDF' => DomPDF\DomPDF\Facade::class,
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
         'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
 
