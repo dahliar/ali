6a64bcf8f47b2a1fddce7f8d6d35b5d57050bae0
dahliar <90829442+dahliar@users.noreply.github.com>
2022-08-04 13:15:27 +0700

    Update info.blade.php

--- resources/views/userMapping/info.blade.php
+++ resources/views/userMapping/info.blade.php
@@ -1,11 +1,8 @@
 <!doctype html>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <html>
 <body>
 <main>
-<div>
 COMMERCIAL INVOICE Document generated at : {{Carbon\Carbon::now()}}
-</div>
 </main>
 </body>
 </html>
