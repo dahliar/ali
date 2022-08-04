40049b03573a227218a405e38350e54260176a29
dahliar <90829442+dahliar@users.noreply.github.com>
2022-08-04 12:42:36 +0700

    Update info.blade.php

--- resources/views/userMapping/info.blade.php
+++ resources/views/userMapping/info.blade.php
@@ -1,13 +1,3 @@
-<!--
-<body>	
-	//echo "PHP Version ".phpversion();
-	// Show all information, defaults to INFO_ALL
-	//phpinfo();
-	// Show just the module information.
-	// phpinfo(8) yields identical results.
-	//phpinfo(INFO_MODULES);
-</body>
--->
 <!doctype html>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<html lang="en">
