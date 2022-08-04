14a6c587a5e41f48476fe30a79e7b3b15be917d4
dahliar <90829442+dahliar@users.noreply.github.com>
2022-08-04 13:18:14 +0700

    Update info.blade.php

--- resources/views/userMapping/info.blade.php
+++ resources/views/userMapping/info.blade.php
@@ -1,1 +1,14 @@
-COMMERCIAL INVOICE Document generated at
+<!doctype html>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<html lang="en">
+	<head>
+		<title>Invoice - 1</title>
+	</head>
+	<body>
+		<main>
+			<div>
+				<h1 align="center">COMMERCIAL INVOICE</h1>
+			</div>
+		</main>
+	</body>
+	</html>
