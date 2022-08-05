fa6cd049b914af46a22aaffbf2c14818989d61e1
dahliar <90829442+dahliar@users.noreply.github.com>
2022-08-05 14:57:37 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,8 +16,7 @@ class DashboardController extends Controller
 
     public function infophp()
     {
-        $x = ["1","2"];
-        $pdf = Pdf::loadview("userMapping.info", $x);
+        $pdf = Pdf::loadview("userMapping.info");
         return $pdf->download("Proforma Invoice.pdf");
 
         //return view('userMapping.info');
