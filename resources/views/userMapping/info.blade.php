f47e06214fe38670b21c28a64da8741a658809c4
dahliar <90829442+dahliar@users.noreply.github.com>
2022-08-05 15:13:58 +0700

    Update info.blade.php

--- resources/views/userMapping/info.blade.php
+++ resources/views/userMapping/info.blade.php
@@ -2,6 +2,9 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<html>
 	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>Invoice - 1</title>
 	</head>
 	<body>
