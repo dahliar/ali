43866560dc38e05e0a371b8ebcd04975bf83c588
dahliar <90829442+dahliar@users.noreply.github.com>
2022-08-05 09:43:22 +0700

    Update slipGajiPegawai.blade.php

--- resources/views/invoice/slipGajiPegawai.blade.php
+++ resources/views/invoice/slipGajiPegawai.blade.php
@@ -44,6 +44,7 @@
     </head>
     <body>
         <header>
+            <!--
             <table width="90%" style="margin-bottom: 0;">
                 <tr>
                     <td width="30%" align="center">
@@ -67,6 +68,7 @@
                     </td>
                 </tr>
             </table>
+        -->
             <hr class="solid" style="width: 90%; margin-top: 0; margin-bottom: 0;">
         </header>
         <main>
