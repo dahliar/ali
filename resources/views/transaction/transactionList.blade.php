c9d9171b5c9ebf5f73a5207a46c4377ab722badd
dahliar <90829442+dahliar@users.noreply.github.com>
2023-02-16 15:25:42 +0700

    Update transactionList.blade.php

--- resources/views/transaction/transactionList.blade.php
+++ resources/views/transaction/transactionList.blade.php
@@ -77,13 +77,13 @@
             type: 'GET',
             destroy:true,
             columnDefs: [
-                {   "width": "7%", "targets":   [0], "className": "text-left" },
-                {   "width": "25%", "targets":  [1], "className": "text-left"   },
+                {   "width": "5%", "targets":   [0], "className": "text-left" },
+                {   "width": "30%", "targets":  [1], "className": "text-left"   },
                 {   "width": "10%", "targets":  [2], "className": "text-left" },
-                {   "width": "18%", "targets":  [3], "className": "text-left" },
-                {   "width": "18%", "targets":  [4], "className": "text-left" },
-                {   "width": "7%", "targets":   [5], "className": "text-left" },
-                {   "width": "15%", "targets":  [6], "className": "text-left" }
+                {   "width": "20%", "targets":  [3], "className": "text-left" },
+                {   "width": "20%", "targets":  [4], "className": "text-left" },
+                {   "width": "10%", "targets":   [5], "className": "text-left" },
+                {   "width": "10%", "targets":  [6], "className": "text-left" }
                 ], 
 
             columns: [
