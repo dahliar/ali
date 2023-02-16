91b6fd8e68c675dbede82c9717202c82cf9243c1
dahliar <90829442+dahliar@users.noreply.github.com>
2023-02-16 15:28:01 +0700

    Update transactionList.blade.php

--- resources/views/transaction/transactionList.blade.php
+++ resources/views/transaction/transactionList.blade.php
@@ -81,8 +81,8 @@
                 {   "width": "25%", "targets":  [1], "className": "text-left"   },
                 {   "width": "10%", "targets":  [2], "className": "text-left" },
                 {   "width": "20%", "targets":  [3], "className": "text-left" },
-                {   "width": "20%", "targets":  [4], "className": "text-left" },
-                {   "width": "10%", "targets":   [5], "className": "text-left" },
+                {   "width": "15%", "targets":  [4], "className": "text-left" },
+                {   "width": "10%", "targets":  [5], "className": "text-left" },
                 {   "width": "15%", "targets":  [6], "className": "text-left" }
                 ], 
 
