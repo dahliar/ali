032b93ce95932e8810fab45ef4fc7a5831974b5b
dahliar <90829442+dahliar@users.noreply.github.com>
2023-02-25 12:11:06 +0700

    Update speciesStockList.blade.php

--- resources/views/item/speciesStockList.blade.php
+++ resources/views/item/speciesStockList.blade.php
@@ -23,13 +23,13 @@
             type: 'GET',
             destroy:true,
             columnDefs: [
-                {   "width": "5%",  "targets":  [0], "className": "text-center" },
-                {   "width": "15%", "targets":  [1], "className": "text-left"   },
-                {   "width": "15%", "targets":  [2], "className": "text-left"   },
-                {   "width": "15%", "targets":  [3], "className": "text-end" },
-                {   "width": "10%", "targets":  [4], "className": "text-end" },
-                {   "width": "10%", "targets":  [5], "className": "text-end" },
-                {   "width": "10%", "targets":  [6], "className": "text-end" }
+                {   "width": "4%",  "targets":  [0], "className": "text-center" },
+                {   "width": "20%", "targets":  [1], "className": "text-left"   },
+                {   "width": "20%", "targets":  [2], "className": "text-left"   },
+                {   "width": "14%", "targets":  [3], "className": "text-end" },
+                {   "width": "14%", "targets":  [4], "className": "text-end" },
+                {   "width": "14%", "targets":  [5], "className": "text-end" },
+                {   "width": "14%", "targets":  [6], "className": "text-end" }
                 ], 
 
             columns: [
